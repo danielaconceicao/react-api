a464099656032a01c445fdf9f3d76f90e2014325
danielaconceicao <daniela.3597@gmail.com>
2024-11-20 17:20:06 +0100

    consuming the blog's api

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,39 @@ import './App.css'
 
 
 function App() {
-  const [formData, setFormData] = useState({})
+  const [postsData, setPostsData] = useState({})
 
 
   useEffect(fetchPost, [])
 
-  function fetchPost(url = 'http://localhost:3001/posts'){
+  function fetchPost(url = 'http://localhost:3001/posts') {
     fetch(url)
-    .then(resp => resp.json())
-    .then(data => {
-      console.log(data)
-      setFormData(data)
-    })
+      .then(resp => resp.json())
+      .then(data => {
+        console.log(data)
+        setPostsData(data)
+      })
   }
 
- 
+
   return (
     <>
       <div className='container'>
-       
-
+        <div className="row">
+          <div className="col">
+            {postsData.data ? postsData.data.map(post => (
+              <div className="card my-4" key={post.id}>
+              <img src={post.image} className="card-img-top"/>
+                <div className="card-body">
+                  <h5 className="card-title">{post.title}</h5>
+                  <p className="card-text">Ricetta: {post.content}</p>
+                  <p className="card-text">Tags: {post.tags.join(', ')}</p>
+                </div>
+            </div>
+            )) : <p></p> }
+            
+          </div>
+        </div>
       </div>
     </>
   )
